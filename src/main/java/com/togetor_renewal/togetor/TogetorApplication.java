c770513378f180ac4cbfbbeff04d35cface6d339
ByungWoo <test@matea.com>
2022-03-13 01:49:42 +0900

    init

---
+++ src/main/java/com/togetor_renewal/togetor/TogetorApplication.java
@@ -0,0 +1,13 @@
+package com.togetor_renewal.togetor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetorApplication.class, args);
+	}
+
+}
